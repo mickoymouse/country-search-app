fbfe10f52f6fe3ae19fe1b1bc9e67e497eeedc72
mickoymouse <30593627+mickoymouse@users.noreply.github.com>
2025-11-01 00:52:42 +0800

    add draft country list

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -1,1 +1,13 @@
-<template><p>list of countries here</p></template>
+<template>
+	<div class="flex justify-center py-6 px-16">
+		<div class="h-full w-full max-w-[1440px] flex flex-col">
+			<div class="w-full flex items-center justify-between">
+				<p>input here</p>
+				<p>filter here</p>
+			</div>
+			<div>
+				<p>list of countries here</p>
+			</div>
+		</div>
+	</div>
+</template>
