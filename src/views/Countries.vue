b57b75c58631dd865fa463b31e4ce762045383e1
mickoymouse <30593627+mickoymouse@users.noreply.github.com>
2025-11-01 17:48:40 +0800

    Add shadcn vue and implement filter by region functionality.

--- src/views/Countries.vue
+++ src/views/Countries.vue
@@ -1,22 +1,57 @@
 <script setup>
-import { onMounted, reactive } from "vue";
+import { onMounted, reactive, ref, watch } from "vue";
 
 import CountryCard from "@/components/CountryCard.vue";
 import { router } from "@/routes";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-vue-next";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
 
 const state = reactive({
 	countries: [],
 });
 
+const regionFilter = ref(null);
+const loadingCountries = ref(false);
+
 const viewCountryInformation = (country) => {
 	router.push(`/country/${country.name.common}`);
 };
 
+const fetchCountries = async (newRegion) => {
+	try {
+		loadingCountries.value = true;
+		if (newRegion) {
+			const res = await fetch(
+				`https://restcountries.com/v3.1/region/${newRegion}?fields=name,cca2,capital,region,borders,flags,population`
+			);
+			state.countries = await res.json();
+		} else {
+			const res = await fetch(
+				"https://restcountries.com/v3.1/all?fields=name,cca2,capital,region,borders,flags,population"
+			);
+			state.countries = await res.json();
+		}
+	} catch (error) {
+		console.error("Error fetching countries:", error);
+	} finally {
+		loadingCountries.value = false;
+	}
+};
+
+watch(regionFilter, async (newRegion) => {
+	fetchCountries(newRegion);
+});
+
 onMounted(async () => {
-	const res = await fetch(
-		"https://restcountries.com/v3.1/all?fields=name,cca2,capital,region,borders,flags,population"
-	);
-	state.countries = await res.json();
+	fetchCountries(null);
 });
 </script>
 
@@ -24,10 +59,42 @@ onMounted(async () => {
 	<div class="flex justify-center py-6 px-20 h-full">
 		<div class="h-full max-w-[1440px] flex flex-col gap-6">
 			<div class="flex items-center justify-between">
-				<p>input here</p>
-				<p>filter here</p>
+				<div class="relative w-[480px]">
+					<Input
+						id="search"
+						type="text"
+						placeholder="Search for a country..."
+						class="pl-10"
+					/>
+					<span
+						class="absolute start-0 inset-y-0 flex items-center justify-center px-2"
+					>
+						<Search class="size-6 text-muted-foreground" />
+					</span>
+				</div>
+				<Select v-model="regionFilter">
+					<SelectTrigger class="w-[200px] min-w-[200px]">
+						<SelectValue placeholder="Filter by region" />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup>
+							<SelectItem value="africa"> Africa </SelectItem>
+							<SelectItem value="america"> America </SelectItem>
+							<SelectItem value="asia"> Asia </SelectItem>
+							<SelectItem value="europe"> Europe </SelectItem>
+							<SelectItem value="oceania"> Oceania </SelectItem>
+						</SelectGroup>
+					</SelectContent>
+				</Select>
 			</div>
-			<div class="flex-1 overflow-auto scrollbar-hide">
+			<div v-if="loadingCountries">
+				<img
+					src="@/assets/undraw_around-the-world_vgcy.svg"
+					alt="loading svg"
+				/>
+				<p>Loading countries...</p>
+			</div>
+			<div v-else class="flex-1 overflow-auto scrollbar-hide">
 				<div class="flex flex-wrap gap-16">
 					<CountryCard
 						class="cursor-pointer"
