47ea496e4a8322ecf6fc85d8ceb9863066677832
mickoymouse <30593627+mickoymouse@users.noreply.github.com>
2025-11-02 20:32:02 +0800

    Change to nav.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,13 +7,15 @@ const { isDarkMode, toggle } = useTheme();
 </script>
 
 <template>
-	<div
+	<nav
 		class="h-20 min-h-20 bg-white dark:bg-(--blue-900) shadow-md w-full flex justify-center"
 	>
 		<div
 			class="h-full w-full max-w-[1440px] flex items-center justify-between px-4 sm:px-20"
 		>
-			<p class="font-extrabold text-[14px] sm:text-2xl">Where in the world?</p>
+			<h1 class="font-extrabold text-[14px] sm:text-2xl">
+				Where in the world?
+			</h1>
 			<button
 				class="text-[12px] sm:text-base font-semibold flex items-center gap-2 cursor-pointer"
 				@click="toggle"
@@ -23,5 +25,5 @@ const { isDarkMode, toggle } = useTheme();
 				{{ isDarkMode ? "Light Mode" : "Dark Mode" }}
 			</button>
 		</div>
-	</div>
+	</nav>
 </template>
